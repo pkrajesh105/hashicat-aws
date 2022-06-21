67749b93d9c60fb23f07e71db9d17f384c04a5f9
Rajesh P <pkrajesh105@outlook.com>
2022-06-21 08:20:58 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,5 @@
 module "s3-bucket" {
   source  = "app.terraform.io/prajesh-training/s3-bucket/aws"
   version = "2.8.0"
-  # insert required variables here
-  prefix = "Bucket_prefix"
+  bucket_prefix = "${var.prefix}"
 }
